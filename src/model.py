42a8bdaf166becfe41d2c105344b70de354f9c00
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-04-06 13:22:01 -0400

    Implemented exact match evaluation metric on model that was trained on smaller datasets

--- src/model.py
+++ src/model.py
@@ -5,45 +5,46 @@ from datasets import DatasetDict, Dataset
 from transformers import TrainingArguments, Trainer
 from transformers import EarlyStoppingCallback
 import pandas as pd
+import torch
+from sklearn.metrics import accuracy_score
 
-# dataset = load_dataset("google/code_x_glue_cc_code_to_code_trans")
-# print("DATASET: ", type(dataset), dataset)
 
-#should also add removing dataframe headers here - should also remove the row index numbers
-train_dataset = pd.read_csv('../data/clean/ft_train.csv')
+if torch.backends.mps.is_available():
+    mps_device = torch.device("mps")
+    x = torch.ones(1, device=mps_device)
+    print(x)
+else:
+    print("MPS device not found")
+
+train_dataset = pd.read_csv('../data/clean/ft_train_small.csv')
 train_dataset = Dataset.from_pandas(train_dataset)
-test_dataset = pd.read_csv('../data/clean/ft_test.csv')
+test_dataset = pd.read_csv('../data/clean/ft_test_small.csv')
 test_dataset = Dataset.from_pandas(test_dataset)
-valid_dataset = pd.read_csv('../data/clean/ft_valid.csv')
+valid_dataset = pd.read_csv('../data/clean/ft_valid_small.csv')
+# train_dataset = pd.read_csv('../data/clean/ft_train.csv')
+# train_dataset = Dataset.from_pandas(train_dataset)
+# test_dataset = pd.read_csv('../data/clean/ft_test.csv')
+# test_dataset = Dataset.from_pandas(test_dataset)
+# valid_dataset = pd.read_csv('../data/clean/ft_valid.csv')
 valid_dataset = Dataset.from_pandas(valid_dataset)
 dataset_dict = DatasetDict({
     'train': train_dataset,
     'validation': valid_dataset,
     'test': test_dataset
 })
-# train_methods = train_dataset['cleaned_method']
-# train_target = train_dataset['target_block']
-# print("TRAIN METHODS: ", train_methods)
-# exit()
-
-# print("Sample Python Code:", dataset['train'][0]['java'])
-# print("Target Java Code:", dataset['train'][0]['cs'])
 
 model_checkpoint = "Salesforce/codet5-small"
 
 model = T5ForConditionalGeneration.from_pretrained(model_checkpoint)
 
+model = model.to(mps_device)
+
 tokenizer = RobertaTokenizer.from_pretrained(model_checkpoint)
-#tokenizer.add_tokens(["<IF-STMT>"]) #Imagine we need an extra token. This line adds the extra token to the vocabulary
 tokenizer.add_tokens(["<TAB>", "<MASK>"])
 
 model.resize_token_embeddings(len(tokenizer))
 
-#edited here
-
 def preprocess_function(examples):
-    # inputs = examples["java"]
-    # targets = examples["cs"]
     inputs = examples['cleaned_method']
     targets = examples['target_block']
     model_inputs = tokenizer(inputs, max_length=256, truncation=True, padding="max_length")
@@ -51,9 +52,21 @@ def preprocess_function(examples):
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
 
-# tokenized_datasets = dataset.map(preprocess_function, batched=True)
-# exit()
 tokenized_datasets = dataset_dict.map(preprocess_function, batched=True)
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions[0]
+    preds = preds.argmax(-1)
+
+    labels = labels.flatten()
+    preds = preds.flatten()
+
+    accuracy = accuracy_score(labels, preds)
+    
+    return {
+        'exact_match': accuracy,
+    }
 
 training_args = TrainingArguments(
     output_dir="./codet5-finetuned",
@@ -70,6 +83,7 @@ training_args = TrainingArguments(
     save_total_limit=2,
     logging_steps=100,
     push_to_hub=False,
+    no_cuda=True
 )
 
 trainer = Trainer(
@@ -78,15 +92,20 @@ trainer = Trainer(
     train_dataset=tokenized_datasets["train"],
     eval_dataset=tokenized_datasets["validation"],
     tokenizer=tokenizer,
+    compute_metrics=compute_metrics,
     callbacks=[EarlyStoppingCallback(early_stopping_patience=2)]
 )
 
 trainer.train()
 
-metrics = trainer.evaluate(tokenized_datasets["test"])
-print("Test Evaluation Metrics:", metrics)
+results = trainer.evaluate()
 
-input_code = "def add(a, b):\n    return a + b"
-inputs = tokenizer(input_code, return_tensors="pt", padding=True, truncation=True)
-outputs = model.generate(**inputs, max_length=256)
-print("Generated Java Code:\n", tokenizer.decode(outputs[0], skip_special_tokens=True))
+print(results)
+
+# metrics = trainer.evaluate(tokenized_datasets["test"])
+# print("Test Evaluation Metrics:", metrics)
+
+# input_code = "def add(a, b):\n    return a + b"
+# inputs = tokenizer(input_code, return_tensors="pt", padding=True, truncation=True)
+# outputs = model.generate(**inputs, max_length=256)
+# print("Generated Java Code:\n", tokenizer.decode(outputs[0], skip_special_tokens=True))
